a3388dab68587f993125f7f18aa9915c3db8b030
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-03-06 16:50:10 -0600

    Add Model Dish.cs

---
+++ StirFryResturant/Models/Dish.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StirFryRestaurant.Models
+{
+    public class Dish
+    {
+        public int DishId { get; set; }
+        public string DishName { get; set; }
+        public string DishDescription { get; set; }
+        public decimal? Price { get; set; }
+        public int CookingTime { get; set; }
+
+    }
+}
